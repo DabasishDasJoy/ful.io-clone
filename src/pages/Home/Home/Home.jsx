f81fc3f807f23ef96f94a4731c68cc0f4d472c10
DabasishDasJoy <dabasishdasjoy92@gmail.com>
2023-02-22 16:00:40 +0600

    info section added

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,41 +1,38 @@
 // import LocalFloristIcon from "@material-ui/icons";
 import styled from "@emotion/styled";
-import { Box, Typography } from "@mui/material";
+import { Box, Link, Typography } from "@mui/material";
 import { Stack } from "@mui/system";
 import React from "react";
-import { Link } from "react-router-dom";
+import SearchWeb from "../SearchWeb/SearchWeb";
 
-const StyledBox = styled(Stack)(({ theme }) => ({
+const InfoWrapper = styled(Stack)(({ theme }) => ({
   display: "flex",
   alignItems: "center",
   justifyContent: "center",
   backgroundColor: "white",
-  padding: theme.spacing(1),
-  borderRadius: "5px",
-  width: "50%",
+  padding: theme.spacing(1, 5),
+  borderRadius: "0.5rem",
   margin: "0 auto",
-  gap: theme.spacing(1),
+  gap: theme.spacing(4),
   flexDirection: "row",
+  maxWidth: "fit-content",
+  boxShadow:
+    "rgb(136 165 191 / 48%) 6px 2px 16px 0px, rgb(255 255 255 / 80%) -6px -2px 16px 0px;",
 }));
-
-// const StyledIcon = styled()({
-//   fontSize: "30px",
-//   color: "green",
-//   marginRight: "10px",
-// });
 
 const Home = () => {
   return (
     <Box>
-      <StyledBox boxShadow={3}>
-        {/* <StyledIcon /> */}
+      <InfoWrapper>
         <Typography variant="body1" component="span">
-          73821 new domains added on 22 February 2023
+          🎉 73821 new domains added on 22 February 2023
         </Typography>
-        <Link href="#" color="primary" underline="always">
+        <Link href="#" color="primary" underline="none">
           Read More
         </Link>
-      </StyledBox>
+      </InfoWrapper>
+
+      <SearchWeb></SearchWeb>
     </Box>
   );
 };
---
+++ src/pages/Home/SearchWeb/SearchWeb.jsx
@@ -0,0 +1,13 @@
+import { Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import React from "react";
+
+const SearchWeb = () => {
+  return (
+    <Box>
+      <Typography>What Is The Web Built On?</Typography>
+    </Box>
+  );
+};
+
+export default SearchWeb;
